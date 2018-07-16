638ca2079ad0a7f282fb55f56222cf89ac13fd5c
Ivan Remmelgas <ivanremmelgas@gmail.com>
2018-07-16 18:01:48 +0300

    auth

--- examples/example1.js
+++ examples/example1.js
@@ -1,10 +1,6 @@
 const jayson = require('jayson');
 
-
-let stream = jayson.client.http({
-    port: 4303
-});
-
+let stream = jayson.client.http({port:4303, headers:{'X-AUTH': 123456}});
 
 stream.request('getMyDeviceWallets', [], (err, state) => {
     if(err)
